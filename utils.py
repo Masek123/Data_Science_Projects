96b10d6490ca65beb395bf581827f27a223e8dad
Szymon <szymon.tomasik201@gmail.com>
2026-09-09 22:54:41 +0200

    Correction of a typo (genres -> genre)

--- utils.py
+++ utils.py
@@ -10,6 +10,6 @@ def parse_names(text):
 def load_clean_data(path='movies_clean.csv'):
     import pandas as pd
     df = pd.read_csv(path)
-    for col in ['genres_list', 'keywords_list', 'cast_list']:
+    for col in ['genre_list', 'keywords_list', 'cast_list']:
         df[col] = df[col].apply(ast.literal_eval)
     return df
